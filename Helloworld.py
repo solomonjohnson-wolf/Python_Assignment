93340f971bf7a9e5ca8d3c24bce7603e907bfced
Solomon <86.ogre_nomads@icloud.com>
2025-07-06 14:12:09 +0000

    Helloworld.py

---
+++ Helloworld.py
@@ -0,0 +1,46 @@
+name = ("Solomon AJ")
+age = 25
+height = 1.75
+is_student = True
+print("Type of name :",type(name))
+print("Type of age :",type(age))
+print("Type of height :",type(height))
+print("Type of is_student :",type(is_student))
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+x = 5
+y = 2.5
+sum_result = x + y
+print(sum_result)
+product_result = x * y
+print(product_result)
+result = x + y
+print(result)
+message ="Solomon AJ"
+print(message[0:-2])
+number1 = input("Enter a number:")
+number2 = input("Enter another number:")
+result = number1 + number2
+print(result)
+number1= int(number1)
+number2 = int(number2)
+result2 = number1 - number2
+print(result2)
+result3 = number1 * number2
+print(result3)
+result4 = number1 / number2
+print(result4)
+
+Patient1 = input("Enter a patient age:")
+Patient2 = input("Enter a patient age:")
+Patient1 = int(Patient1)
+Patient2 = int(Patient2)
+if Patient1 > Patient2:
+    print("Patient1 is older than Patient2")
+elif Patient1 < Patient2:
+    print("Patient2 is older than Patient1")
+
+
+
