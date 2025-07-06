153c34cdf8caec01db062d8108cbea82ba348947
Solomon <86.ogre_nomads@icloud.com>
2025-07-06 14:23:33 +0000

    lets try again

--- mosh.py
+++
@@ -1,7 +0,0 @@
-patient = "john smith"
-age = 35
-is_new = True
-name = input("What is your name?")
-print("Hi" + name )
-colour = input("What is your favourite color?")
-print(name + " likes " + colour)
